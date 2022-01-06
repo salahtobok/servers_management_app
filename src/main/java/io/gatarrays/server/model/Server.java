332266e217177a6c75e7a744181450c73a13e8a0
salaheddine.tobok <salaheddine.tobok@eadn.dz>
2022-01-06 14:55:45 +0100

    Upload application core files .

--- src/main/java/io/gatarrays/server/model/Server.java
+++ src/main/java/io/gatarrays/server/model/Server.java
@@ -1,9 +1,26 @@
 package io.gatarrays.server.model;
 
 import io.gatarrays.server.enumeration.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+import static javax.persistence.GenerationType.AUTO;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Server {
+    @Id
+    @GeneratedValue(strategy = AUTO)
     private Long id;
+    @Column(unique = true)
+    @NotEmpty(message = "ip Address cannot be empty or null")
     private String ipAddress;
     private String name;
     private String memory;
