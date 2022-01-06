abe9bd4959dd6c157a76a8659abb9417bf2a360f
salaheddine.tobok <salaheddine.tobok@eadn.dz>
2022-01-06 13:47:25 +0100

    Upload application core files .

---
+++ src/main/java/io/gatarrays/server/enumeration/Status.java
@@ -0,0 +1,23 @@
+package io.gatarrays.server.enumeration;
+
+public enum Status {
+
+    SERVER_UP("SERVER_UP"),
+    SERVER_DOWN("SERVER_DOWN");
+
+    private final String status;
+
+    Status(String status) {
+        this.status = status;
+    }
+
+
+    public String getStatus(){
+        return this.status;
+    }
+
+/*    public void setStatus(String status){
+        this.status = status;
+    }*/
+
+}
---
+++ src/main/java/io/gatarrays/server/model/Server.java
@@ -0,0 +1,14 @@
+package io.gatarrays.server.model;
+
+import io.gatarrays.server.enumeration.Status;
+
+public class Server {
+    private Long id;
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+
+}
