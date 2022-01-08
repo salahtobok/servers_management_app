b856786c19ed7453a5c03d3f5e053d14709ceb95
salaheddine.tobok <salaheddine.tobok@eadn.dz>
2022-01-08 20:57:01 +0100

    Update files .

--- src/main/webapp/serverapp/src/app/app.component.ts
+++ src/main/webapp/serverapp/src/app/app.component.ts
@@ -17,7 +17,10 @@ export class AppComponent implements OnInit {
   readonly DataState = DataState;
   readonly Status = Status;
   private filterSubject = new BehaviorSubject<string>('');
+  // @ts-ignore
+  private dataSubject = new BehaviorSubject<CustomResponse>(null);
   filterStatus$ = this.filterSubject.asObservable();
+
   constructor(private serverService: ServerService) {
   }
 
@@ -25,10 +28,33 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.appState$ = this.serverService.servers$.pipe(
       map(response => {
+          this.dataSubject.next(response);
           return {dataState: DataState.LOADED_STATE, appData: response}
         }
       ),
       startWith({dataState: DataState.LOADING_STATE}),
+      catchError((error: string) => {
+        return of({dataState: DataState.ERROR_STATE, error})
+      })
+    )
+  }
+
+
+  pingServer(ipAddress: string): void {
+    this.filterSubject.next(ipAddress);
+    this.appState$ = this.serverService.ping$(ipAddress)
+      .pipe(
+      map(response => {
+          // @ts-ignore
+        const index = this.dataSubject.value.data.servers.findIndex(server => server.id === response.data.server.id)
+        if (response.data.server) {
+          this.dataSubject.value.data.servers[index] = response.data.server;
+        }
+        this.filterSubject.next('')
+          return {dataState: DataState.LOADED_STATE, appData: response}
+        }
+      ),
+      startWith({dataState: DataState.LOADED_STATE, appData: this.dataSubject.value}),
       catchError((error: string) => {
         return of({dataState: DataState.ERROR_STATE, error})
       })
